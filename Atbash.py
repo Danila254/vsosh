15dbd4bf5786127c247e5fa61da71889a84371f2
Danila254 <danila.2010kuznetsov.2010@gmail.com>
2024-07-17 19:02:42 +0400

    Add files via upload

--- Atbash.py
+++ Atbash.py
@@ -1,10 +1,24 @@
 class Atbash:
     def encrypt(text):
-         alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧШЩЪЫЬЭЮЯ'
-         alpha = 'абвгдеёжзийклмнопрстуфхчшщъыьэюя'
-        
-         for i in text:
-             if i.islower():
-                 text(alpha)
-                 else:
-                     text(alphabet)
+        alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧШЩЪЫЬЭЮЯ'
+        alpha = 'абвгдеёжзийклмнопрстуфхчшщъыьэюя'
+        result = ""
+        for i in text:
+            if i.islower():
+                result += Atbash.func(text)
+            else:
+                result += Atbash.funcC(text)
+        print(result)
+
+    def func(text):
+        alphabet = 'абвгдеёжзийклмнопрстуфхчшщъыьэюя'     
+        cipher = {alphabet: alphabet[::i-1] for i in range(len(alphabet))}
+        return "".join(cipher.get(symbol, symbol) for symbol in text)
+
+    def funcC(text):
+        alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧШЩЪЫЬЭЮЯ'    
+        cipher = {alphabet: alphabet[::i-1] for i in range(len(alphabet))}
+        return "".join(cipher.get(symbol, symbol) for symbol in text)
+
+
+Atbash.encrypt("Привет")
